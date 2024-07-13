a035e0184be34856d4aee279f757eac1f4d55bdb
JFreeman00 <jakobblomdahl@hotmail.com>
2024-07-13 11:18:17 +0200

    lade till en SmartHomeController class

---
+++ src/main/java/com/example/WebServer/SmartHome/SmartHomeController.java
@@ -0,0 +1,4 @@
+package com.example.WebServer.SmartHome;
+
+public class SmartHomeController {
+}
--- src/main/java/com/example/WebServer/WebServerApplication.java
+++ src/main/java/com/example/WebServer/WebServerApplication.java
@@ -3,6 +3,11 @@ package com.example.WebServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main
+ * HTTP Message Converter
+ */
+
 @SpringBootApplication
 public class WebServerApplication {
 
