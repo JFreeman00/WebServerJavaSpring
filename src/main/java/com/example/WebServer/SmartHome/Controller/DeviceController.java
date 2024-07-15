a85bff7cd2886ecfd8359c1a5e9c9a9b7edc515a
JFreeman00 <jakobblomdahl@hotmail.com>
2024-07-15 14:27:44 +0200

    Flyttade våran on/ off check till service klassen

--- src/main/java/com/example/WebServer/SmartHome/Controller/DeviceController.java
+++ src/main/java/com/example/WebServer/SmartHome/Controller/DeviceController.java
@@ -15,34 +15,14 @@ public class DeviceController {
     @Autowired
     private DeviceService deviceService;
 
+    // ResponseEntity<Object>
     @PostMapping("/send_data")
     public ResponseEntity<Object> receiveMessage(@RequestBody DeviceCommand deviceCommand) {
 
         System.out.println("Received command: " + deviceCommand.getCommand());
         //System.out.println("For device: " + deviceCommand.getDevice());
+        return deviceService.sendMessageToDevice(deviceCommand);
 
-        // TODO flytta denna check till DeviceService
-        if("on".equals(deviceCommand.getCommand())) {
-            deviceService.sendMessageToDevice(deviceCommand.getCommand());
-            //------------------------------ allt under ska över till DeviceService
-            deviceCommand.setCommand("on");
-            String response = String.format("Command sent to ESP: %s", deviceCommand.getCommand());
-            StatusResponse validResponse = new StatusResponse("success", response);
-            return ResponseEntity.ok().body(validResponse);
-        }
-
-        else if("off".equals(deviceCommand.getCommand())) {
-            deviceService.sendMessageToDevice(deviceCommand.getCommand());
-            //---------------------------- allt under ska över till DeviceService
-            deviceCommand.setCommand("off");
-            String response = String.format("Command sent to ESP: %s", deviceCommand.getCommand());
-            StatusResponse validResponse = new StatusResponse("success", response);
-            return ResponseEntity.ok().body(validResponse);
-        }
-        else{
-            StatusResponse errorResponse = new StatusResponse("error", "Failed to send command to device");
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-        }
     }
 
     // Här ska vi retunera information
--- src/main/java/com/example/WebServer/SmartHome/Service/DeviceService.java
+++ src/main/java/com/example/WebServer/SmartHome/Service/DeviceService.java
@@ -2,6 +2,7 @@ package com.example.WebServer.SmartHome.Service;
 
 import com.example.WebServer.SmartHome.Controller.DeviceController;
 import com.example.WebServer.SmartHome.DeviceCommand;
+import com.example.WebServer.SmartHome.Entity.StatusResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -14,48 +15,74 @@ public class DeviceService {
     // We use 1337 that's why we need to specify the port number in the URL
     // http://192.168.0.97 for ESP
     private final String ESP_URL = "http://192.168.0.97:1337";
-    private final DeviceController deviceController;
+    //private final DeviceController deviceController;
 
-    public DeviceService(DeviceController deviceController) {
+    /*public DeviceService(DeviceController deviceController) {
         this.deviceController = deviceController;
     }
 
+     */
 
-    public void sendMessageToDevice(String command) {
+    public ResponseEntity<Object> sendMessageToDevice(DeviceCommand deviceCommand) {
 
-        // Lägga till checken för om det ör on eller off
-        //checkMessage(command);
+        ResponseEntity<Object> responseCheckMessage = checkMessage(deviceCommand);
 
-        RestTemplate restTemplate = new RestTemplate();
+        if(responseCheckMessage.getStatusCode() == HttpStatus.OK) {
+            RestTemplate restTemplate = new RestTemplate();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // Det vi ska skicka (Payload)
-        String jsonBody = "{\"command\": \"" + command + "\"}";
+            // Det vi ska skicka (Payload) så här måste vi skicka en string
+            String jsonBody = "{\"command\": \"" + deviceCommand.getCommand() + "\"}";
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
+            HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
 
-        String URL = ESP_URL + "/process_data";
+            String URL = ESP_URL + "/process_data";
 
-        // Here we try to send the payload to the esp
-        try {
-            ResponseEntity<String> response = restTemplate.exchange(URL,HttpMethod.POST, requestEntity, String.class);
-            System.out.println("Response: " + response.getStatusCode());
-            System.out.println("Response body: " + response.getBody());
+            // Here we try to send the payload to the esp
+            try {
+                ResponseEntity<String> response = restTemplate.exchange(URL,HttpMethod.POST, requestEntity, String.class);
+                System.out.println("Response: " + response.getStatusCode());
+                System.out.println("Response body: " + response.getBody());
+            }
+            catch (Exception e) {
+                System.err.println("Error sending command to ESP32: " + e.getMessage());
+            }
+            return new ResponseEntity<>(new StatusResponse("success", "Command sent to ESP"), HttpStatus.OK);
         }
-        catch (Exception e) {
-            System.err.println("Error sending command to ESP32: " + e.getMessage());
-        }
 
+        return responseCheckMessage;
     }
 
-    public String checkMessage(String command) {
+    public ResponseEntity<Object> checkMessage(DeviceCommand deviceCommand) {
 
-        if("on".equals(command)) {
-            DeviceCommand deviceCommand = new DeviceCommand();
+        // Kommer nog aldrig att vara denna
+        if(deviceCommand.getCommand() == null || deviceCommand.getCommand().isEmpty()) {
+            return new ResponseEntity<>("Command is missing", HttpStatus.BAD_REQUEST);
         }
 
-        return "";
+        if("on".equals(deviceCommand.getCommand())) {
+            deviceCommand.setCommand("on");
+            return new ResponseEntity<>("Command is valid", HttpStatus.OK);
+
+            //String response = String.format("Command sent to ESP: %s", deviceCommand.getCommand());
+            //StatusResponse validResponse = new StatusResponse("success", response);
+            //return ResponseEntity.ok().body(validResponse);
+
+        } else if ("off".equals(deviceCommand.getCommand())) {
+            deviceCommand.setCommand("off");
+            return new ResponseEntity<>("Command is valid", HttpStatus.OK);
+
+            //String response = String.format("Command sent to ESP: %s", deviceCommand.getCommand());
+            //StatusResponse validResponse = new StatusResponse("success", response);
+            //return ResponseEntity.ok().body(validResponse);
+        }
+
+        else{
+            //StatusResponse errorResponse = new StatusResponse("error", "Failed to send command to device");
+            //return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+            return new ResponseEntity<>(new StatusResponse("error", "Failed to send command to device"), HttpStatus.BAD_REQUEST);
+        }
     }
 }
