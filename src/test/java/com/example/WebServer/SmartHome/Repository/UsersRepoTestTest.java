9c5c2caa15b8a70889173dd27b8523ab243930b8
YungFreeman <jakobblomdahl@hotmail.com>
2024-08-11 09:59:23 +0200

    kommenterade ut koden i test filerna

---
+++ src/test/java/com/example/WebServer/SmartHome/Repository/UsersRepoTestTest.java
@@ -0,0 +1,44 @@
+package com.example.WebServer.SmartHome.Repository;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Optional;
+
+//import com.example.WebServer.SmartHome.Service.UsersService;
+import org.junit.jupiter.api.Test;
+
+//import com.example.WebServer.SmartHome.Entity.Users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+/**
+ * Test class to see if email search works
+ */
+@SpringBootTest
+public class UsersRepoTestTest {
+
+
+    /*@Test
+    public void testValidEmails() {
+        assertTrue(UsersService.emailValidator("test@example.com"));
+        assertTrue(UsersService.emailValidator("user.name+tag+sorting@example.com"));
+        assertTrue(UsersService.emailValidator("user@sub.example.com"));
+    }
+
+    /*@Test
+    public void testInvalidEmails() {
+        assertFalse(UsersService.emailValidator("plainaddress"));
+        assertFalse(UsersService.emailValidator("@missingusername.com"));
+        assertFalse(UsersService.emailValidator("username@.com"));
+        assertFalse(UsersService.emailValidator("username@com"));
+        assertFalse(UsersService.emailValidator("username@.com."));
+    }
+
+     */
+
+
+
+}
